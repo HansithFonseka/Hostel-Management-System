99875aa3dfb5d9d6f5069a7236dcb2e21480718e
Hansith <hansithlochana@gmail.com>
2024-09-17 13:19:17 +0530

    Create a new class

---
+++ StudentHostel.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentHostel {
+
+    public static void main(String[] args) {
+
+        ArrayList<Student> students = new ArrayList<>();
+        final int TOTAL_ROOMS = 50;
+
+        System.out.println("--- Student Hostel Management System ---");
+        Scanner scanner = new Scanner(System.in);
+    }
+}
