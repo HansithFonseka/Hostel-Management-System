90d3b7a68cdaf8f005f7362a36b7f4dc87bb7b91
Hansith <hansithlochana@gmail.com>
2024-09-17 13:09:12 +0530

    constructor

---
+++ Student.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Student {
+    String name;
+    int RoomNumber;
+    int ListOfPayments;
+
+    public Student(String name, int roomNumber, int listOfPayments) {
+        this.name = name;
+        this.RoomNumber = roomNumber;
+        this.ListOfPayments = listOfPayments;
+    }
+}
